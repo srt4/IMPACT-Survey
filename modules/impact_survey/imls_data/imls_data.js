8131f3005dee38d91ce0331e74da2c1544c3e69e
mortona <mortona@uw.edu>
2011-05-06 13:23:45 -0700

    Fixed imls data editing.

--- modules/impact_survey/imls_data/imls_data.js
+++ modules/impact_survey/imls_data/imls_data.js
@@ -2,10 +2,10 @@
 
 (function($){
 $(document).ready(function(){
-	$('#profile-imls-data-field-sq-ft-add-more-wrapper a').attr('href', '');
-	$('#profile-imls-data-field-sq-ft-add-more-wrapper a').removeAttr('target');
+	$('.square_feet_link').attr('href', '#');
+	$('.square_feet_link').removeAttr('target');
 	var openwin;
-	$('#profile-imls-data-field-sq-ft-add-more-wrapper a').click(function() {
+	$('.square_feet_link').click(function() {
 		openwin=window.open('http://localhost/impact_local/square_feet','loading','height=300,width=300,scrollbars=yes,toolbar=no,location=no,status=no,menubar=no,resizable=no,dependent=no');
 	});
 });
