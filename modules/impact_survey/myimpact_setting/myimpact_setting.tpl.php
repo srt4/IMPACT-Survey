d0e1a4c5f56961f1dbf2393f688fc67018f74b6d
yanfang <yanfang@uw.edu>
2011-05-27 17:08:07 -0700

    the interface of the myimpact

---
+++ modules/impact_survey/myimpact_setting/myimpact_setting.tpl.php
@@ -0,0 +1,32 @@
+
+<div id="left_content" style="float:left;">
+
+	<h3><?php if(isset($libName)) print $libName;?></h3>
+	
+	Username: <?php print $user->name;?><br>
+	
+	Registered User: <?php  if(isset($firstName, $lastName)) print $firstName." ".$lastName; ?><br>
+	
+	Position: <?php  if(isset($jobtitle)) print $jobtitle; ?><br>
+	
+	<?php if(isset($phoneNum)) print substr($phoneNum,0,3)."-".substr($phoneNum,3,3)."-".substr($phoneNum,-(strlen($phoneNum)-6)).$phoneExt; ?><br>
+	
+	<?php print $user->mail;?>
+	
+	<h3>Completed Surveys:  <span style='color:#333333'><?php if(isset($surveyCount)) print $surveyCount;?></span></h3><p>*Updated every 24 hours.</p>
+	
+	<input type="button" value="Edit" class="form-submit" onClick="window.location.href='myimpact/edit'" /> <br>
+		
+	<h4>Next Step</h4>
+	
+	<?php if(isset($step)) print $step;?>
+
+</div><!-- left_content -->
+
+<div id='photo_logo'>
+
+	<?php print $pic_output;?>
+
+	<div align='center'><input type='button' class="form-submit" value="<?php if(isset($tag)) print $tag;?>" onClick="window.location.href = '<?php print "./profile-photo_logo/$user->uid/edit";?>'" /></div>
+
+</div><!-- photo_logo -->
